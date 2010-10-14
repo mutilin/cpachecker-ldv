mutilin/cpachecker-ldv

commit 028ea17ec08b9fd0b0373e19582f9ffeb304f160

Don't try to parse float constants with Long.parseLong(). This fixes a NFE with constants like 0x1.4p+4 (yes, this is a valid float constant). Thanks to Karlheinz for finding this.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2077 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/symbpredabstraction/CtoFormulaConverter.java
@@ -617,7 +617,6 @@ public class CtoFormulaConverter {
       String num = lexp.getRawSignature();
       switch (lexp.getKind()) {
       case IASTLiteralExpression.lk_integer_constant:
-      case IASTLiteralExpression.lk_float_constant:
         if (num.startsWith("0x")) {
           // this should be in hex format
           // we use Long instead of Integer to avoid getting negative
@@ -632,7 +631,26 @@ public class CtoFormulaConverter {
           }
           num = num.substring(0, pos+1);
         }
-        break;
+        
+        // TODO here we assume 32 bit integers!!! This is because CIL
+        // seems to do so as well...
+        try {
+          Integer.parseInt(num);
+        } catch (NumberFormatException nfe) {
+          long l = Long.parseLong(num);
+          if (l < 0) {
+            num = Long.toString(Integer.MAX_VALUE + l);
+          } else {
+            num = Long.toString(l - ((long)Integer.MAX_VALUE + 1)*2);
+          }
+        }
+        return smgr.makeNumber(num);
+        
+      case IASTLiteralExpression.lk_float_constant:
+        // parse with valueOf and convert to String again, because Mathsat
+        // does not accept all possible C float constants (but Java hopefully does)
+        return smgr.makeNumber(Double.valueOf(num).toString());
+
       case IASTLiteralExpression.lk_char_constant: {
         // we convert to a byte, and take the integer value
         String s = exp.getRawSignature();
@@ -667,19 +685,7 @@ public class CtoFormulaConverter {
       default:
         throw new UnrecognizedCCodeException("Unknown literal", null, exp);
       }
-      // TODO here we assume 32 bit integers!!! This is because CIL
-      // seems to do so as well...
-      try {
-        Integer.parseInt(num);
-      } catch (NumberFormatException nfe) {
-        long l = Long.parseLong(num);
-        if (l < 0) {
-          num = Long.toString(Integer.MAX_VALUE + l);
-        } else {
-          num = Long.toString(l - ((long)Integer.MAX_VALUE + 1)*2);
-        }
-      }
-      return smgr.makeNumber(num);
+
     } else if (exp instanceof IASTCastExpression) {
       // we completely ignore type casts
       logger.log(Level.ALL, "DEBUG_3", "IGNORING TYPE CAST:",

commit 08c106fcf629245c91764a56a907f60801342066

fix handling of char constants (broken by r2077)
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2091 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/symbpredabstraction/CtoFormulaConverter.java
@@ -665,10 +665,9 @@ public class CtoFormulaConverter {
           assert (exp.getRawSignature().length() == 3);
           n = exp.getRawSignature().charAt(1);
         }
-        num = "" + n;
-
+        return smgr.makeNumber("" + n);
       }
-      break;
+
       case IASTLiteralExpression.lk_string_literal: {
         // we create a string constant representing the given
         // string literal
@@ -769,6 +768,7 @@ public class CtoFormulaConverter {
         return smgr.makeIfThenElse(ftmp, smgr.makeNumber(1), smgr.makeNumber(0));
       }
       }
+
     } else if (exp instanceof IASTBinaryExpression) {
       int op = ((IASTBinaryExpression)exp).getOperator();
       IASTExpression e1 = ((IASTBinaryExpression)exp).getOperand1();
@@ -865,8 +865,9 @@ public class CtoFormulaConverter {
           return smgr.makeShiftLeft(me1, me2);
         case IASTBinaryExpression.op_shiftRight:
           return smgr.makeShiftRight(me1, me2);
+        default:
+          throw new AssertionError("Missing switch case");
         }
-        break;
       }
 
       default:
@@ -876,6 +877,7 @@ public class CtoFormulaConverter {
         SymbolicFormula ftmp = makePredicate(exp, true, function, ssa);
         return smgr.makeIfThenElse(ftmp, smgr.makeNumber(1), smgr.makeNumber(0));
       }
+
     } else if (exp instanceof IASTFieldReference) {
       if (lvalsAsUif) {
         IASTFieldReference fexp = (IASTFieldReference)exp;
@@ -920,9 +922,10 @@ public class CtoFormulaConverter {
         IASTTypeIdExpression.op_sizeof);
       warnUnsafeVar(exp);
       return makeVariable(exprToVarName(exp), function, ssa);
+
+    } else {
+      throw new UnrecognizedCCodeException("Unknown expression", null, exp);
     }
-    
-    throw new UnrecognizedCCodeException("Unknown expression", null, exp);
   }
 
   private SymbolicFormula buildLvalueTerm(IASTExpression exp,
@@ -1000,8 +1003,10 @@ public class CtoFormulaConverter {
       int idx = makeLvalIndex(ufname, args, ssa);
 
       return smgr.makeUIF(ufname, args, idx);
+
+    } else {
+      throw new UnrecognizedCCodeException("Unknown lvalue", null, exp);
     }
-    throw new UnrecognizedCCodeException("Unknown lvalue", null, exp);
   }
 
   private SymbolicFormula makeExternalFunctionCall(IASTFunctionCallExpression fexp,