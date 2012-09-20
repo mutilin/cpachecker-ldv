f08dec37749742c7f9399bca66878713407e3f65
Vadim Mutilin <mutilin@ispras.ru>
2012-09-20 18:42:58 +0400

    do not hangup if cil.out.i file not found

--- src/org/sosy_lab/cpachecker/cpa/usageStatistics/CodeCoveringSimple.java
+++ src/org/sosy_lab/cpachecker/cpa/usageStatistics/CodeCoveringSimple.java
@@ -56,11 +56,9 @@ public class CodeCoveringSimple implements CodeCovering{
 
   @Override
   public void generate() {
-    PrintWriter writer = null;
-    FileOutputStream file = null;
     boolean usedFunction = false, globalDeclaration = true;
     int lineCounter = 0;
-    final String folder = "/home/alpha/git/cpachecker/test/";
+    final String folder = "/home/osuser/ispras/";
     final String filename = "cil.out";
     final String inputFile = filename + ".i";
     final String outputFile = filename + ".info";
@@ -71,78 +69,81 @@ public class CodeCoveringSimple implements CodeCovering{
     UsedFunctions.add(cfa.getMainFunction().getFunctionName());
 
     try {
-      file = new FileOutputStream (folder + outputFile);
-      writer = new PrintWriter(file);
-    } catch(FileNotFoundException e) {
-      System.out.println("Cannot open file " + folder + outputFile);
-      System.exit(0);
-    }
+        BufferedReader in = new BufferedReader(new FileReader(folder + inputFile));
+      
+        try {
+            FileOutputStream file = new FileOutputStream (folder + outputFile);
+            PrintWriter writer = new PrintWriter(file);
 
-    writer.println("TN:");
-    writer.println("SF:" + folder + inputFile);
-
-    for (FunctionEntryNode entry : cfa.getAllFunctionHeads()) {
-      functionHeads.put(entry.getLineNumber(), entry.getFunctionName());
-      writer.println("FN:" + entry.getLineNumber() + "," + entry.getFunctionName());
-    }
-
-    for (String name : UsedFunctions) {
-      if (cfa.getAllFunctionNames().contains(name)) {
-        writer.println("FNDA:" + "1," + name);
-        FNH++;
-      }
-    }
-
-    writer.println("FNF:" + cfa.getNumberOfFunctions());
-    writer.println("FNH:" + FNH);
-
-    for (CFANode node : cfa.getAllNodes()) {
-      for (int i = 0; i < node.getNumEnteringEdges(); i++)
-        lineUsage.add(node.getEnteringEdge(i).getLineNumber());
-      for (int i = 0; i < node.getNumLeavingEdges(); i++)
-        lineUsage.add(node.getLeavingEdge(i).getLineNumber());
-    }
-
-    try {
-      BufferedReader in = new BufferedReader(new FileReader(folder + inputFile));
-      String str;
-      while ((str = in.readLine()) != null) {
-        lineCounter++;
-
-        if (str.matches("#.*") || (str.matches(" *\\{ *")) || str.matches(" *\\} *") ||
-            str.matches(" *//.*") || str.matches(" */\\*.*\\*/ *") || str.matches(" *"))
-          continue;
-
-        if (functionHeads.containsKey(lineCounter)) {
-          String functionName = functionHeads.get(lineCounter);
-          globalDeclaration = false;
-          if ((usedFunction = UsedFunctions.contains(functionName)) == true) {
-            writer.println("DA:" + lineCounter +",1");
-          } else {
-            writer.println("DA:" + lineCounter +",0");
-          }
-
-        } else if (lineUsage.contains(lineCounter)) {
-          if (usedFunction)
-            writer.println("DA:" + lineCounter +",1");
-          else if (!globalDeclaration)
-            writer.println("DA:" + lineCounter +",0");
-
-        } else if (!str.contains("else") && !str.contains("goto") && !str.contains(":") && !str.equals("") && !globalDeclaration) {
-            writer.println("DA:" + lineCounter + ",0");
+            writer.println("TN:");
+            writer.println("SF:" + folder + inputFile);
+	
+		    for (FunctionEntryNode entry : cfa.getAllFunctionHeads()) {
+		      functionHeads.put(entry.getLineNumber(), entry.getFunctionName());
+		      writer.println("FN:" + entry.getLineNumber() + "," + entry.getFunctionName());
+		    }
+		
+		    for (String name : UsedFunctions) {
+		      if (cfa.getAllFunctionNames().contains(name)) {
+		        writer.println("FNDA:" + "1," + name);
+		        FNH++;
+		      }
+		    }
+		
+		    writer.println("FNF:" + cfa.getNumberOfFunctions());
+		    writer.println("FNH:" + FNH);
+		
+		    for (CFANode node : cfa.getAllNodes()) {
+		      for (int i = 0; i < node.getNumEnteringEdges(); i++)
+		        lineUsage.add(node.getEnteringEdge(i).getLineNumber());
+		      for (int i = 0; i < node.getNumLeavingEdges(); i++)
+		        lineUsage.add(node.getLeavingEdge(i).getLineNumber());
+		    }
+	
+		      String str;
+		      while ((str = in.readLine()) != null) {
+		        lineCounter++;
+		
+		        if (str.matches("#.*") || (str.matches(" *\\{ *")) || str.matches(" *\\} *") ||
+		            str.matches(" *//.*") || str.matches(" */\\*.*\\*/ *") || str.matches(" *"))
+		          continue;
+		
+		        if (functionHeads.containsKey(lineCounter)) {
+		          String functionName = functionHeads.get(lineCounter);
+		          globalDeclaration = false;
+		          if ((usedFunction = UsedFunctions.contains(functionName)) == true) {
+		            writer.println("DA:" + lineCounter +",1");
+		          } else {
+		            writer.println("DA:" + lineCounter +",0");
+		          }
+		
+		        } else if (lineUsage.contains(lineCounter)) {
+		          if (usedFunction)
+		            writer.println("DA:" + lineCounter +",1");
+		          else if (!globalDeclaration)
+		            writer.println("DA:" + lineCounter +",0");
+		
+		        } else if (!str.contains("else") && !str.contains("goto") && !str.contains(":") && !str.equals("") && !globalDeclaration) {
+		            writer.println("DA:" + lineCounter + ",0");
+		        }
+		    }
+		    
+		    writer.println("LF:" + lineCounter);
+		    writer.println("LH:" + lineUsage.size());
+		    writer.println("end_of_record");
+		    writer.close();
+        } catch(FileNotFoundException e) {
+        	System.err.println("Cannot open output file " + folder + outputFile);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        } finally {
+        	in.close();		 
         }
-      }
-      in.close();
+    } catch(FileNotFoundException e) {
+    	System.out.println("Cannot open input file " + folder + inputFile);
+    } catch(IOException e) {
+    	e.printStackTrace();
     }
-    catch (IOException e) {
-      System.out.println("Cannot open file " + folder + inputFile);
-      writer.close();
-      System.exit(0);
-    }
-    writer.println("LF:" + lineCounter);
-    writer.println("LH:" + lineUsage.size());
-    writer.println("end_of_record");
-    writer.close();
   }
-
+  
 }
