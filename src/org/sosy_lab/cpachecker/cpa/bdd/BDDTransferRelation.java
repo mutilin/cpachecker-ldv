mutilin/cpachecker-ldv

commit 607606b173fbfa56e2c172285ee7a5cb1839b558

refactoring and bugfixes for BDDCPA
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10981 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/bdd/PredicateManager.java
@@ -1,0 +1,188 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.bdd;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.util.VariableClassification;
+import org.sosy_lab.cpachecker.util.predicates.NamedRegionManager;
+import org.sosy_lab.cpachecker.util.predicates.interfaces.Region;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/** This class guarantees a fixed order of variables in the BDD,
+ * that should be good for the operations in the BitvectorManager. */
+@Options(prefix = "cpa.bdd")
+public class PredicateManager {
+
+  @Option(description = "declare first bit of all vars, then second bit,...")
+  private boolean initBitwise = true;
+
+  @Option(description = "declare the bits of a var from 0 to N or from N to 0")
+  private boolean initBitsIncreasing = true;
+
+  @Option(description = "declare partitions ordered")
+  private boolean initPartitionsOrdered = true;
+
+  @Option(description = "declare vars partitionwise")
+  private boolean initPartitions = true;
+
+  protected static final String TMP_VARIABLE = "__CPAchecker_tmp_var";
+  private final Map<Multimap<String, String>, String> varsToTmpVar = new HashMap<>();
+
+  /** Contains the varNames of all really tracked vars.
+   * This set may differ from the union of all partitions,
+   * because not every variable, that appears in the sourcecode,
+   * is analyzed or even reachable. */
+  private final Multimap<String, String> trackedVars = LinkedHashMultimap.create();
+
+  private final NamedRegionManager rmgr;
+
+  public PredicateManager(final Configuration config, final NamedRegionManager pRmgr,
+                          final BDDPrecision pPrecision, final CFA pCfa,
+                          final BDDTransferRelation pTransferRelation
+                          ) throws InvalidConfigurationException {
+    config.inject(this);
+    this.rmgr = pRmgr;
+
+    if (initPartitions) {
+      initVars(pPrecision, pCfa, pTransferRelation);
+    }
+  }
+
+  public Multimap<String, String> getTrackedVars() {
+    return trackedVars;
+  }
+
+  /** return a specific temp-variable, that should be at correct positions in the BDD. */
+  public String getTmpVariableForVars(final Multimap<String, String> vars) {
+    if (initPartitions) {
+      return varsToTmpVar.get(vars);
+    } else {
+      return TMP_VARIABLE;
+    }
+  }
+
+
+  /** The BDDRegionManager orders the variables as they are declared
+   *  (later vars are deeper in the BDD).
+   *  This function declares those vars in the beginning of the analysis,
+   *  so that we can choose between some orders. */
+  protected void initVars(BDDPrecision precision, CFA cfa, BDDTransferRelation transferRelation) {
+    List<VariableClassification.Partition> partitions;
+    if (initPartitionsOrdered) {
+      BDDPartitionOrderer d = new BDDPartitionOrderer(cfa);
+      partitions = d.getOrderedPartitions();
+    } else {
+      assert cfa.getVarClassification().isPresent();
+      partitions = cfa.getVarClassification().get().getPartitions(); // may be unsorted
+    }
+
+    for (VariableClassification.Partition partition : partitions) {
+      // maxBitSize is too much for most variables. we only create an order here, so this should not matter.
+      createPredicates(partition.getVars(), precision, transferRelation.getMaxBitsize());
+    }
+  }
+
+  /** This function declares variables for a given collection of vars.
+   *
+   * The value 'bitsize' chooses how much bits are used for each var.
+   * The varname is build as "varname@pos". */
+  public void createPredicates(final Multimap<String, String> vars, final BDDPrecision precision, final int bitsize) {
+
+    assert bitsize >= 1 : "you need at least one bit for a variable.";
+
+    // add a temporary variable for each set of variables, introducing it here is cheap, later it may be  expensive.
+    String tmpVar = TMP_VARIABLE + "_" + varsToTmpVar.size();
+    varsToTmpVar.put(vars, tmpVar);
+
+    // bitvectors [a2, a1, a0]
+    // 'initBitwise' chooses between initialing each var separately or bitwise overlapped.
+    if (initBitwise) {
+
+      // [a2, b2, c2, a1, b1, c1, a0, b0, c0]
+      boolean isTrackingSomething = false;
+      for (int i = 0; i < bitsize; i++) {
+        int index = initBitsIncreasing ? i : (bitsize - i - 1);
+        for (Map.Entry<String, String> entry : vars.entries()) {
+          if (precision.isTracking(entry.getKey(), entry.getValue())) {
+            createPredicateDirectly(entry.getKey(), entry.getValue(), index);
+            isTrackingSomething = true;
+          }
+        }
+        if (isTrackingSomething) {
+          createPredicateDirectly(null, tmpVar, index);
+        }
+      }
+
+    } else {
+      // [a2, a1, a0, b2, b1, b0, c2, c1, c0]
+      boolean isTrackingSomething = false;
+      for (Map.Entry<String, String> entry : vars.entries()) { // different loop order!
+        if (precision.isTracking(entry.getKey(), entry.getValue())) {
+          for (int i = 0; i < bitsize; i++) {
+            int index = initBitsIncreasing ? i : (bitsize - i - 1);
+            createPredicateDirectly(entry.getKey(), entry.getValue(), index);
+          }
+          isTrackingSomething = true;
+        }
+      }
+      if (isTrackingSomething) {
+        for (int i = 0; i < bitsize; i++) {
+          int index = initBitsIncreasing ? i : (bitsize - i - 1);
+          createPredicateDirectly(null, tmpVar, index);
+        }
+      }
+    }
+  }
+
+  /** This function returns a region for a variable.
+   * This function does not track any statistics. */
+  private Region createPredicateDirectly(@Nullable final String functionName, final String varName, final int index) {
+    System.out.println(((functionName == null) ? varName : functionName + "::" + varName) + "@" + index);
+    return rmgr.createPredicate(((functionName == null) ? varName : functionName + "::" + varName) + "@" + index);
+  }
+
+  /** This function returns regions containing bits of a variable.
+   * returns regions for positions of a variable, s --> [s@2, s@1, s@0].
+   * There is no check, if the variable is tracked by the the precision. */
+  public Region[] createPredicates(@Nullable final String functionName, final String varName, final int size) {
+    trackedVars.put(functionName, varName);
+    final Region[] newRegions = new Region[size];
+    for (int i = size - 1; i >= 0; i--) {
+      // inverse order should be faster, because 'most changing bits' are at bottom position in BDDs.
+      newRegions[i] = createPredicateDirectly(functionName, varName, i);
+    }
+    return newRegions;
+  }
+}

src/org/sosy_lab/cpachecker/cpa/bdd/BDDBooleanExpressionVisitor.java
@@ -1,0 +1,141 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.bdd;
+
+import org.sosy_lab.cpachecker.cfa.ast.c.*;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression.UnaryOperator;
+import org.sosy_lab.cpachecker.cfa.types.c.CEnumType.CEnumerator;
+import org.sosy_lab.cpachecker.util.predicates.interfaces.Region;
+import org.sosy_lab.cpachecker.util.predicates.interfaces.RegionManager;
+
+/** This Visitor implements evaluation of simply typed expressions.
+ * This Visitor is specialized for boolean expressions. */
+public class BDDBooleanExpressionVisitor
+        extends DefaultCExpressionVisitor<Region, RuntimeException> {
+
+  private final static int BOOLEAN_SIZE = 1;
+  private final BDDTransferRelation transferRelation;
+  protected final RegionManager rmgr;
+
+  /** This Visitor returns the boolean value for an expression. */
+  protected BDDBooleanExpressionVisitor(final BDDTransferRelation pTransferRelation, final RegionManager pRmgr) {
+    this.transferRelation = pTransferRelation;
+    this.rmgr = pRmgr;
+  }
+
+  @Override
+  protected Region visitDefault(CExpression pExp) {
+    return null;
+  }
+
+  @Override
+  public Region visit(final CBinaryExpression pE) {
+    final Region lVal = pE.getOperand1().accept(this);
+    final Region rVal = pE.getOperand2().accept(this);
+    if (lVal == null || rVal == null) {
+      return null;
+    }
+    return calculateBinaryOperation(lVal, rVal, rmgr, pE);
+  }
+
+  public static Region calculateBinaryOperation(Region l, Region r, final RegionManager rmgr,
+                                                final CBinaryExpression binaryExpr) {
+
+    final BinaryOperator binaryOperator = binaryExpr.getOperator();
+    switch (binaryOperator) {
+      case BINARY_AND:
+        return rmgr.makeAnd(l, r);
+      case BINARY_OR:
+        return rmgr.makeOr(l, r);
+      case BINARY_XOR:
+      case NOT_EQUALS:
+        return rmgr.makeUnequal(l, r);
+      case EQUALS:
+        return rmgr.makeEqual(l, r);
+      default:
+        throw new AssertionError("unhandled binary operator");
+    }
+  }
+
+  @Override
+  public Region visit(CCharLiteralExpression pE) {
+    return getNum(pE.getCharacter());
+  }
+
+  @Override
+  public Region visit(CIntegerLiteralExpression pE) {
+    return getNum(pE.asLong());
+  }
+
+  @Override
+  public Region visit(CImaginaryLiteralExpression pE) {
+    return pE.getValue().accept(this);
+  }
+
+  @Override
+  public Region visit(CIdExpression idExp) {
+    if (idExp.getDeclaration() instanceof CEnumerator) {
+      CEnumerator enumerator = (CEnumerator) idExp.getDeclaration();
+      if (enumerator.hasValue()) {
+        return getNum(enumerator.getValue());
+      } else {
+        return null;
+      }
+    }
+
+    return transferRelation.createPredicate(idExp, BOOLEAN_SIZE)[0];
+  }
+
+  private Region getNum(long num) {
+    if (num == 0) {
+      return rmgr.makeFalse();
+    } else if (num == 1) {
+      return rmgr.makeTrue();
+    } else {
+      throw new AssertionError("no boolean value: " + num);
+    }
+  }
+
+  @Override
+  public Region visit(final CUnaryExpression unaryExpression) {
+    final UnaryOperator unaryOperator = unaryExpression.getOperator();
+    final Region value = unaryExpression.getOperand().accept(this);
+
+    if (value == null) {
+      return null;
+    }
+
+    switch (unaryOperator) {
+      case PLUS:
+        return value;
+
+      case NOT:
+        return rmgr.makeNot(value);
+
+      default:
+        return null;
+    }
+  }
+}

src/org/sosy_lab/cpachecker/cpa/bdd/BDDCompressExpressionVisitor.java
@@ -1,0 +1,192 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.bdd;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
+import org.sosy_lab.cpachecker.cfa.ast.c.*;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression.UnaryOperator;
+import org.sosy_lab.cpachecker.cfa.types.c.CEnumType.CEnumerator;
+import org.sosy_lab.cpachecker.util.VariableClassification;
+import org.sosy_lab.cpachecker.util.VariableClassification.Partition;
+import org.sosy_lab.cpachecker.util.predicates.interfaces.Region;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This Visitor implements evaluation of expressions,
+ * that contain only checks for equality of numbers.
+ * In this special case it is possible to store the information in less bits than 32.
+ */
+public class BDDCompressExpressionVisitor
+        extends DefaultCExpressionVisitor<Region[], RuntimeException> {
+
+  /** This map contains tuples (int, region[]) for each intEqual-partition. */
+  private static final Map<Partition, Map<BigInteger, Region[]>> INT_REGIONS_MAP = new HashMap<>();
+
+  private final BDDTransferRelation transferRelation;
+  private final BitvectorManager bvmgr;
+  private final Map<BigInteger, Region[]> intToRegions;
+  private final int size;
+
+  /** This Visitor returns a representation for an expression.
+   * @param pTransferRelation needed for variableNames
+   * @param pPartition info about variables and numbers
+   */
+  public BDDCompressExpressionVisitor(final BDDTransferRelation pTransferRelation,
+                                      final BitvectorManager pBVmgr, final Partition pPartition) {
+    Preconditions.checkNotNull(pPartition);
+    this.transferRelation = pTransferRelation;
+    this.bvmgr = pBVmgr;
+    this.size = transferRelation.getBitsize(pPartition, null);
+    this.intToRegions = initMappingIntToRegions(pPartition);
+  }
+
+  /** This function creates a mapping of intEqual partitions to a mapping of number to bitvector.
+   * This allows to compress big numbers to a small number of bits in the BDD. */
+  private Map<BigInteger, Region[]> initMappingIntToRegions(final Partition partition) {
+
+    if (!INT_REGIONS_MAP.containsKey(partition)) {
+      final Map<BigInteger, Region[]> currentMapping = new HashMap<>();
+
+      // special handling of One and Zero,
+      // because they can appear as result of an equality-check.
+      // this allows us to check expressions as "((a==0)==5)" with varClass intEQ
+      currentMapping.put(BigInteger.ZERO, bvmgr.makeNumber(BigInteger.valueOf(0), size));
+      currentMapping.put(BigInteger.ONE, bvmgr.makeNumber(BigInteger.valueOf(1), size));
+
+      int i = 2;
+      for (BigInteger num : Sets.difference(
+              partition.getValues(), Sets.newHashSet(BigInteger.ZERO, BigInteger.ONE))) {
+        currentMapping.put(num, bvmgr.makeNumber(BigInteger.valueOf(i), size));
+        i++;
+      }
+      INT_REGIONS_MAP.put(partition, currentMapping);
+    }
+    return INT_REGIONS_MAP.get(partition);
+  }
+
+  @Override
+  protected Region[] visitDefault(CExpression pExp) {
+    return null;
+  }
+
+  @Override
+  public Region[] visit(final CBinaryExpression pE) {
+
+    // for numeral values
+    Region[] lVal;
+    BigInteger val1 = VariableClassification.getNumber(pE.getOperand1());
+    if (val1 == null) {
+      lVal = pE.getOperand1().accept(this);
+    } else {
+      lVal = intToRegions.get(val1);
+      assert lVal != null;
+    }
+
+    // for numeral values
+    BigInteger val2 = VariableClassification.getNumber(pE.getOperand2());
+    Region[] rVal;
+    if (val2 == null) {
+      rVal = pE.getOperand2().accept(this);
+    } else {
+      rVal = intToRegions.get(val2);
+      assert rVal != null;
+    }
+
+    if (lVal == null || rVal == null) {
+      return null;
+    }
+
+    return calculateBinaryOperation(lVal, rVal, bvmgr, pE);
+  }
+
+  private static Region[] calculateBinaryOperation(Region[] lVal, Region[] rVal, final BitvectorManager bvmgr,
+                                                   final CBinaryExpression binaryExpr) {
+
+    assert lVal.length == rVal.length;
+    final BinaryOperator binaryOperator = binaryExpr.getOperator();
+    switch (binaryOperator) {
+      case EQUALS:
+        return bvmgr.wrapLast(bvmgr.makeLogicalEqual(lVal, rVal), lVal.length);
+      case NOT_EQUALS:
+        return bvmgr.wrapLast(bvmgr.makeNot(bvmgr.makeLogicalEqual(lVal, rVal)), lVal.length);
+      default:
+        throw new AssertionError("no support for further operators: " + binaryOperator);
+    }
+  }
+
+  @Override
+  public Region[] visit(CCharLiteralExpression pE) {
+    return intToRegions.get(BigInteger.valueOf(pE.getCharacter()));
+  }
+
+  @Override
+  public Region[] visit(CIntegerLiteralExpression pE) {
+    return intToRegions.get(pE.getValue());
+  }
+
+  @Override
+  public Region[] visit(CIdExpression idExp) {
+    if (idExp.getDeclaration() instanceof CEnumerator) {
+      CEnumerator enumerator = (CEnumerator) idExp.getDeclaration();
+      if (enumerator.hasValue()) {
+        return intToRegions.get(BigInteger.valueOf(enumerator.getValue()));
+      } else {
+        return null;
+      }
+    }
+    return transferRelation.createPredicate(idExp, size);
+  }
+
+  @Override
+  public Region[] visit(final CUnaryExpression unaryExpression) {
+
+    // for numeral values
+    final BigInteger val = VariableClassification.getNumber(unaryExpression);
+    if (val != null) {
+      return intToRegions.get(val);
+    }
+
+    final UnaryOperator unaryOperator = unaryExpression.getOperator();
+    final CExpression unaryOperand = unaryExpression.getOperand();
+    final Region[] value = unaryOperand.accept(this);
+
+    if (value == null) {
+      return null;
+    }
+
+    switch (unaryOperator) {
+      case PLUS:
+        return value;
+      case NOT:
+        return bvmgr.wrapLast(bvmgr.makeNot(value), value.length);
+      default:
+        throw new AssertionError("no support for further operators: " + unaryOperator);
+    }
+  }
+}

commit 376d804e239c0d2e514ea281390e39e9470f8503

bugfixes for BDDCPA: first bug was a too small number of initial nodes (8 vs 64, introduced in r10981), that leads to a very bad order of variables in the BDD, second bug was a change in the Precision (unknown revision), that results in an empty set of initial nodes.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@11440 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/bdd/BDDTransferRelation.java
@@ -114,7 +114,7 @@ public class BDDTransferRelation extends ForwardingTransferRelation<BDDState, BD
     this.machineModel = cfa.getMachineModel();
     this.rmgr = manager;
     this.bvmgr = new BitvectorManager(config, rmgr);
-    this.predmgr = new PredicateManager(config, manager, precision, cfa, this);
+    this.predmgr = new PredicateManager(config, manager, precision, cfa, machineModel);
 
     assert cfa.getVarClassification().isPresent();
     this.varClass = cfa.getVarClassification().get();

src/org/sosy_lab/cpachecker/cpa/bdd/PredicateManager.java
@@ -34,6 +34,7 @@ import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.types.MachineModel;
 import org.sosy_lab.cpachecker.util.VariableClassification;
 import org.sosy_lab.cpachecker.util.predicates.NamedRegionManager;
 import org.sosy_lab.cpachecker.util.predicates.interfaces.Region;
@@ -71,13 +72,13 @@ public class PredicateManager {
 
   public PredicateManager(final Configuration config, final NamedRegionManager pRmgr,
                           final BDDPrecision pPrecision, final CFA pCfa,
-                          final BDDTransferRelation pTransferRelation
+                          final MachineModel pMachineModel
                           ) throws InvalidConfigurationException {
     config.inject(this);
     this.rmgr = pRmgr;
 
     if (initPartitions) {
-      initVars(pPrecision, pCfa, pTransferRelation);
+      initVars(pPrecision, pCfa, pMachineModel);
     }
   }
 
@@ -99,7 +100,7 @@ public class PredicateManager {
    *  (later vars are deeper in the BDD).
    *  This function declares those vars in the beginning of the analysis,
    *  so that we can choose between some orders. */
-  protected void initVars(BDDPrecision precision, CFA cfa, BDDTransferRelation transferRelation) {
+  protected void initVars(BDDPrecision precision, CFA cfa, MachineModel pMachineModel) {
     List<VariableClassification.Partition> partitions;
     if (initPartitionsOrdered) {
       BDDPartitionOrderer d = new BDDPartitionOrderer(cfa);
@@ -111,7 +112,8 @@ public class PredicateManager {
 
     for (VariableClassification.Partition partition : partitions) {
       // maxBitSize is too much for most variables. we only create an order here, so this should not matter.
-      createPredicates(partition.getVars(), precision, transferRelation.getMaxBitsize());
+      createPredicates(partition.getVars(), precision,
+              pMachineModel.getSizeofLongLongInt() * pMachineModel.getSizeofCharInBits());
     }
   }
 
@@ -136,10 +138,8 @@ public class PredicateManager {
       for (int i = 0; i < bitsize; i++) {
         int index = initBitsIncreasing ? i : (bitsize - i - 1);
         for (Map.Entry<String, String> entry : vars.entries()) {
-          if (precision.isTracking(entry.getKey(), entry.getValue())) {
-            createPredicateDirectly(entry.getKey(), entry.getValue(), index);
-            isTrackingSomething = true;
-          }
+          createPredicateDirectly(entry.getKey(), entry.getValue(), index);
+          isTrackingSomething = true;
         }
         if (isTrackingSomething) {
           createPredicateDirectly(null, tmpVar, index);
@@ -150,11 +150,9 @@ public class PredicateManager {
       // [a2, a1, a0, b2, b1, b0, c2, c1, c0]
       boolean isTrackingSomething = false;
       for (Map.Entry<String, String> entry : vars.entries()) { // different loop order!
-        if (precision.isTracking(entry.getKey(), entry.getValue())) {
-          for (int i = 0; i < bitsize; i++) {
-            int index = initBitsIncreasing ? i : (bitsize - i - 1);
-            createPredicateDirectly(entry.getKey(), entry.getValue(), index);
-          }
+        for (int i = 0; i < bitsize; i++) {
+          int index = initBitsIncreasing ? i : (bitsize - i - 1);
+          createPredicateDirectly(entry.getKey(), entry.getValue(), index);
           isTrackingSomething = true;
         }
       }