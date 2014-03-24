98f3a72628111592236be5072bc6984ba28e0ff4
Pavel <andrianov@ispras.ru>
2014-03-24 15:23:51 +0400

    Remove predicate information

--- src/org/sosy_lab/cpachecker/cpa/predicate/RefinementStrategy.java
+++ src/org/sosy_lab/cpachecker/cpa/predicate/RefinementStrategy.java
@@ -40,7 +40,6 @@ import org.sosy_lab.cpachecker.cpa.arg.ARGReachedSet;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.exceptions.RefinementFailedException;
-import org.sosy_lab.cpachecker.util.AbstractStates;
 import org.sosy_lab.cpachecker.util.Precisions;
 import org.sosy_lab.cpachecker.util.predicates.Solver;
 import org.sosy_lab.cpachecker.util.predicates.interfaces.BooleanFormula;
@@ -190,8 +189,8 @@ public abstract class RefinementStrategy {
           differentNontrivialItps++;
         }
       }
-      System.out.println("State: " + AbstractStates.extractLocation(w) + "(" +
-          AbstractStates.extractLocation(w).getLineNumber() + ")" + " <-> " + itp);
+      /*System.out.println("State: " + AbstractStates.extractLocation(w) + "(" +
+          AbstractStates.extractLocation(w).getLineNumber() + ")" + " <-> " + itp);*/
       lastItp = itp;
 
       nonTrivialStates++;
