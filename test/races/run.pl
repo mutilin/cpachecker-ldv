86ffd831e004d16a7b185c565604fce227afdacc
Pavel <andrianov@ispras.ru>
2015-02-03 16:40:45 +0400

    Posibility to run choosen test

--- test/races/run.pl
+++ test/races/run.pl
@@ -14,11 +14,9 @@ my @tool_failure_names;
 my $new_tests = 0;
 my @new_test_names;
 
-system("rm -rf output");
-system("mkdir output");
-foreach my $test_file (glob($prefix."/test/races/src/*.c"))
-{
-#my $test_file = $prefix."/test/races/src/RaceTest5.c";
+
+sub run_test {
+    my $test_file = shift;
     my $ideal_path = $test_file;
 	$ideal_path =~ m|.*\/(\w+).c|;
     my $name = $1;
@@ -54,20 +52,37 @@ foreach my $test_file (glob($prefix."/test/races/src/*.c"))
         print "WARNING: Tool failed on test $name!\n";
     }
 }
-print "\n";
-print "RESULT: Test set is finished\n";
-print "RESULT: Passed tests: $passed_tests\n";
-if ( $failed_tests > 0) {
-    print "RESULT: Failed tests: $failed_tests\n";
-    print "@failed_test_names\n";
-}
-if ($new_tests > 0) {
-    print "RESULT: New tests: $new_tests\n";
-    print "@new_test_names\n";
-}
-if ($tool_failures > 0) {
-    print "RESULT: Tool failures: $tool_failures\n";
-    print "@tool_failure_names\n";
+
+system("rm -rf output");
+system("mkdir output");
+
+my $target_test = $ARGV[0];
+if (defined($target_test)) {
+    if ( -e $target_test) {
+        run_test($target_test);
+    } else {
+        die("Test $target_test is not found\n");
+    }
+} else {
+    foreach my $test_file (glob($prefix."/test/races/src/*.c"))
+    {
+        run_test($test_file);
+    }
+    print "\n";
+    print "RESULT: Test set is finished\n";
+    print "RESULT: Passed tests: $passed_tests\n";
+    if ( $failed_tests > 0) {
+        print "RESULT: Failed tests: $failed_tests\n";
+        print "@failed_test_names\n";
+    }
+    if ($new_tests > 0) {
+        print "RESULT: New tests: $new_tests\n";
+        print "@new_test_names\n";
+    }
+    if ($tool_failures > 0) {
+        print "RESULT: Tool failures: $tool_failures\n";
+        print "@tool_failure_names\n";
+    }
 }
 print "You may look the difference, like\n";
 print "meld output/unsafe_rawdata_NAME_OF_TEST.new ideal_verdict/NAME_OF_TEST.c\n";
