mutilin/cpachecker-ldv

commit e2dd897015c3a4069106f9c48d2498b7ab1d9e4f

Changed #buildRelation such that it is able to cope with wholes in ARGPaths
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@18664 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/refinement/UseDefRelation.java
@@ -25,11 +25,13 @@ package org.sosy_lab.cpachecker.util.refinement;
 
 import static com.google.common.base.Predicates.*;
 import static com.google.common.collect.Collections2.filter;
+import static org.sosy_lab.cpachecker.util.AbstractStates.extractLocation;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -174,9 +176,14 @@ public class UseDefRelation {
 
   private void buildRelation(ARGPath path) {
     PathIterator iterator = path.reversePathIterator();
-    while (iterator.hasNext()) {
-      iterator.advance();
-      CFAEdge edge = iterator.getOutgoingEdge();
+    Iterator<CFAEdge> fullPath = Lists.reverse(path.getFullPath()).iterator();
+
+    // consume last edge (it is the edge to the error and therefore not useful here)
+    fullPath.next();
+    iterator.advance();
+
+    while (fullPath.hasNext()) {
+      CFAEdge edge = fullPath.next();
 
       if (edge.getEdgeType() == CFAEdgeType.MultiEdge) {
         for (CFAEdge singleEdge : Lists.reverse(((MultiEdge)edge).getEdges())) {
@@ -185,6 +192,12 @@ public class UseDefRelation {
       }
       else {
         updateUseDefRelation(iterator.getAbstractState(), edge);
+      }
+
+      // if the edge leads to location which equals to the location of the abstract
+      // state we have to advance the iterator for one position
+      if (edge.getSuccessor().equals(extractLocation(iterator.getAbstractState()))) {
+        iterator.advance();
       }
 
       // stop the traversal once a fix-point is reached

commit 90885bfea0894cf43e93e6e6a8fda897628470e5

Fixing bug introduced in r18664
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@18665 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/refinement/UseDefRelation.java
@@ -178,26 +178,24 @@ public class UseDefRelation {
     PathIterator iterator = path.reversePathIterator();
     Iterator<CFAEdge> fullPath = Lists.reverse(path.getFullPath()).iterator();
 
-    // consume last edge (it is the edge to the error and therefore not useful here)
-    fullPath.next();
+    // move from last state to second last, this is the first interesting one
     iterator.advance();
 
     while (fullPath.hasNext()) {
       CFAEdge edge = fullPath.next();
 
-      if (edge.getEdgeType() == CFAEdgeType.MultiEdge) {
-        for (CFAEdge singleEdge : Lists.reverse(((MultiEdge)edge).getEdges())) {
-          updateUseDefRelation(iterator.getAbstractState(), singleEdge);
-        }
-      }
-      else {
-        updateUseDefRelation(iterator.getAbstractState(), edge);
-      }
-
       // if the edge leads to location which equals to the location of the abstract
       // state we have to advance the iterator for one position
       if (edge.getSuccessor().equals(extractLocation(iterator.getAbstractState()))) {
         iterator.advance();
+      }
+
+      if (edge.getEdgeType() == CFAEdgeType.MultiEdge) {
+        for (CFAEdge singleEdge : Lists.reverse(((MultiEdge)edge).getEdges())) {
+          updateUseDefRelation(iterator.getAbstractState(), singleEdge);
+        }
+      } else {
+        updateUseDefRelation(iterator.getAbstractState(), edge);
       }
 
       // stop the traversal once a fix-point is reached