mutilin/cpachecker-ldv

commit 91b0ca0c2c98ec9a32ba286f15ae517d1b97afd0

Explicit interface to model properties (Property). Integrated it into CPAchecker 'trunk' from the branch 'muauto'.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@18480 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonTransferRelation.java
@@ -59,7 +59,6 @@ import org.sosy_lab.cpachecker.util.statistics.StatIntHist;
 import org.sosy_lab.cpachecker.util.statistics.StatKind;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
@@ -266,8 +265,11 @@ class AutomatonTransferRelation extends SingleEdgeTransferRelation {
 
           } else {
             // matching transitions, but unfulfilled assertions: goto error state
-            String violatedPropertyDescription = Strings.nullToEmpty(t.getViolatedPropertyDescription(exprArgs));
-            AutomatonState errorState = AutomatonState.automatonStateFactory(Collections.<String, AutomatonVariable>emptyMap(), AutomatonInternalState.ERROR, cpa, 0, 0, violatedPropertyDescription);
+            AutomatonSafetyProperty prop = new AutomatonSafetyProperty(state.getOwningAutomaton(), t);
+
+            AutomatonState errorState = AutomatonState.automatonStateFactory(
+                Collections.<String, AutomatonVariable>emptyMap(), AutomatonInternalState.ERROR, cpa, 0, 0, prop);
+
             logger.log(Level.INFO, "Automaton going to ErrorState on edge \"" + edge.getDescription() + "\"");
             lSuccessors.add(errorState);
           }
@@ -295,11 +297,15 @@ class AutomatonTransferRelation extends SingleEdgeTransferRelation {
         exprArgs.putTransitionVariables(transitionVariables);
         t.executeActions(exprArgs);
         actionTime.stop();
-        String violatedPropertyDescription = null;
+
+        AutomatonSafetyProperty violatedProperty = null;
         if (t.getFollowState().isTarget()) {
-          violatedPropertyDescription = t.getViolatedPropertyDescription(exprArgs);
+          violatedProperty = new AutomatonSafetyProperty(state.getOwningAutomaton(), t);
         }
-        AutomatonState lSuccessor = AutomatonState.automatonStateFactory(newVars, t.getFollowState(), cpa, t.getAssumptions(), state.getMatches() + 1, state.getFailedMatches(), violatedPropertyDescription);
+
+        AutomatonState lSuccessor = AutomatonState.automatonStateFactory(
+            newVars, t.getFollowState(), cpa, t.getAssumptions(), state.getMatches() + 1, state.getFailedMatches(), violatedProperty);
+
         if (!(lSuccessor instanceof AutomatonState.BOTTOM)) {
           lSuccessors.add(lSuccessor);
         } else {

src/org/sosy_lab/cpachecker/core/CPAchecker.java
@@ -35,8 +35,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 
-import javax.annotation.Nullable;
-
 import org.sosy_lab.common.AbstractMBean;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.ShutdownNotifier.ShutdownRequestListener;
@@ -63,6 +61,7 @@ import org.sosy_lab.cpachecker.core.algorithm.impact.ImpactAlgorithm;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.Property;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
 import org.sosy_lab.cpachecker.core.interfaces.Targetable;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
@@ -73,7 +72,6 @@ import org.sosy_lab.cpachecker.util.LoopStructure.Loop;
 import org.sosy_lab.cpachecker.util.automaton.TargetLocationProvider;
 import org.sosy_lab.cpachecker.util.globalinfo.GlobalInfo;
 
-import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.base.StandardSystemProperty;
@@ -283,7 +281,12 @@ public class CPAchecker {
 
       AlgorithmStatus status = runAlgorithm(algorithm, reached, stats);
 
-      violatedPropertyDescription = findViolatedProperties(reached);
+      violatedPropertyDescription = null;
+      Set<Property> violatedProperties = findViolatedProperties(algorithm, reached);
+      if (!violatedProperties.isEmpty()) {
+        violatedPropertyDescription = Joiner.on(", ").join(violatedProperties);
+      }
+
       if (violatedPropertyDescription != null) {
         if (!status.isPrecise()) {
           result = Result.UNKNOWN;
@@ -408,21 +411,17 @@ public class CPAchecker {
     }
   }
 
-  private @Nullable String findViolatedProperties(final ReachedSet reached) {
-    Set<String> descriptions = from(reached).filter(IS_TARGET_STATE)
-                        .transform(new Function<AbstractState, String>() {
-                                    @Override
-                                    public String apply(AbstractState s) {
-                                      return  ((Targetable)s).getViolatedPropertyDescription();
-                                    }
-                                  })
-                        .copyInto(new HashSet<String>());
-    if (descriptions.isEmpty()) {
-      // signal no target state -> result safe
-      return null;
+  private Set<Property> findViolatedProperties(final Algorithm pAlgorithm,
+      final ReachedSet reached) {
+
+    final Set<Property> result = Sets.newHashSet();
+
+    for (AbstractState e : from(reached).filter(IS_TARGET_STATE).toList()) {
+      Targetable t = (Targetable) e;
+      result.addAll(t.getViolatedProperties());
     }
-    descriptions.remove("");
-    return Joiner.on(", ").join(descriptions);
+
+    return result;
   }
 
   private Result analyzeResult(final ReachedSet reached, boolean isSound) {

src/org/sosy_lab/cpachecker/core/algorithm/ExternalCBMCAlgorithm.java
@@ -28,6 +28,7 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
@@ -40,12 +41,15 @@ import org.sosy_lab.common.time.Timer;
 import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
 import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.Property;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.core.interfaces.StatisticsProvider;
 import org.sosy_lab.cpachecker.core.interfaces.Targetable;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.util.CBMCExecutor;
+
+import com.google.common.collect.ImmutableSet;
 
 @Options()
 public class ExternalCBMCAlgorithm implements Algorithm, StatisticsProvider {
@@ -174,7 +178,17 @@ public class ExternalCBMCAlgorithm implements Algorithm, StatisticsProvider {
     }
   }
 
+  private static class CbmcReachabilityProperty implements Property {
+
+    @Override
+    public String toString() {
+      return "Target location reachabable with CBMC!";
+    }
+  }
+
   private static class DummyErrorState implements AbstractState, Targetable {
+
+    private final Property prop = new CbmcReachabilityProperty();
 
     @Override
     public boolean isTarget() {
@@ -182,8 +196,8 @@ public class ExternalCBMCAlgorithm implements Algorithm, StatisticsProvider {
     }
 
     @Override
-    public String getViolatedPropertyDescription() throws IllegalStateException {
-      return "";
+    public Set<Property> getViolatedProperties() throws IllegalStateException {
+      return ImmutableSet.of(prop);
     }
   }
 }

src/org/sosy_lab/cpachecker/core/defaults/AbstractSingleWrapperState.java
@@ -26,12 +26,14 @@ package org.sosy_lab.cpachecker.core.defaults;
 import static com.google.common.base.Preconditions.checkState;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.annotation.Nullable;
 
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractWrapperState;
 import org.sosy_lab.cpachecker.core.interfaces.Partitionable;
+import org.sosy_lab.cpachecker.core.interfaces.Property;
 import org.sosy_lab.cpachecker.core.interfaces.Targetable;
 
 import com.google.common.base.Function;
@@ -82,9 +84,9 @@ public abstract class AbstractSingleWrapperState implements AbstractWrapperState
   }
 
   @Override
-  public String getViolatedPropertyDescription() throws IllegalStateException {
+  public Set<Property> getViolatedProperties() throws IllegalStateException {
     checkState(isTarget());
-    return ((Targetable)wrappedState).getViolatedPropertyDescription();
+    return ((Targetable)wrappedState).getViolatedProperties();
   }
 
   @Override

src/org/sosy_lab/cpachecker/core/defaults/NamedProperty.java
@@ -2,7 +2,7 @@
  *  CPAchecker is a tool for configurable software verification.
  *  This file is part of CPAchecker.
  *
- *  Copyright (C) 2007-2014  Dirk Beyer
+ *  Copyright (C) 2007-2015  Dirk Beyer
  *  All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,44 +21,48 @@
  *  CPAchecker web page:
  *    http://cpachecker.sosy-lab.org
  */
-package org.sosy_lab.cpachecker.util.error;
+package org.sosy_lab.cpachecker.core.defaults;
 
 import java.util.Set;
 
-import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Property;
-import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 
 
-public class DummyErrorState extends ARGState {
+public final class NamedProperty implements Property {
 
-  private static final Property dummyProperty = new Property() {
-    @Override
-    public String toString() {
-      return "DummyProperty";
+  private final String text;
+
+  private NamedProperty(String pText) {
+    this.text = Preconditions.checkNotNull(pText);
+  }
+
+  @Override
+  public int hashCode() {
+    return text.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object pOther) {
+    if (!(pOther instanceof NamedProperty)) {
+      return false;
     }
-  };
-
-  private static final long serialVersionUID = 1338393013733003150L;
-
-  public DummyErrorState(final AbstractState pWrapped) {
-    super(pWrapped, null);
+    return this.text.equals(pOther.toString());
   }
 
   @Override
-  public boolean isTarget() {
-    return true;
+  public String toString() {
+    return text;
   }
 
-  @Override
-  public Set<Property> getViolatedProperties() throws IllegalStateException {
-    return ImmutableSet.of(dummyProperty);
+  public static NamedProperty create(final String pText) {
+    return new NamedProperty(pText);
   }
 
-  @Override
-  public Object getPartitionKey() {
-    return null;
+  public static Set<Property> singleton(final String pText) {
+    return ImmutableSet.<Property>of(NamedProperty.create(pText));
   }
+
 }

src/org/sosy_lab/cpachecker/core/interfaces/Property.java
@@ -2,7 +2,7 @@
  *  CPAchecker is a tool for configurable software verification.
  *  This file is part of CPAchecker.
  *
- *  Copyright (C) 2007-2014  Dirk Beyer
+ *  Copyright (C) 2007-2015  Dirk Beyer
  *  All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,25 +23,23 @@
  */
 package org.sosy_lab.cpachecker.core.interfaces;
 
-import java.util.Set;
-
-import javax.annotation.Nonnull;
 
 /**
- * This interface is provided as a shortcut, so that other CPAs' strengthen
- * operator can check whether one abstract state represents some kind of
- * "target" or "error" abstract state without needing to know more about the state
- * (especially without knowing its actual type).
+ * This interface represents a property that is checked
+ * during the analysis.
+ *
+ * A specification might consist of a set of properties.
+ *
+ * Instances of this interface...
+ *    MUST override the .toString() method to provide a description of the property!
+ *    MIGHT override the .equals(...) method! (and implicitly the hashCode() method)
  */
-public interface Targetable {
-
-  public boolean isTarget();
+public interface Property {
 
   /**
-   * Return a human-readable description of the violated property.
-   * Example: "assert statement in line X"
-   * @return A non-null String, may be empty if no description is available.
-   * @throws IllegalStateException if {@link #isTarget()} returns false
+   * @return  The textual description of the property.
    */
-  public @Nonnull Set<Property> getViolatedProperties() throws IllegalStateException;
+  @Override
+  public String toString();
+
 }

src/org/sosy_lab/cpachecker/cpa/arg/ARGPathExporter.java
@@ -27,6 +27,7 @@ import static org.sosy_lab.cpachecker.util.automaton.AutomatonGraphmlCommon.SINK
 
 import java.io.IOException;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
@@ -89,6 +90,7 @@ import org.sosy_lab.cpachecker.core.CounterexampleInfo;
 import org.sosy_lab.cpachecker.core.counterexample.CFAEdgeWithAssumptions;
 import org.sosy_lab.cpachecker.core.counterexample.CFAPathWithAssumptions;
 import org.sosy_lab.cpachecker.core.counterexample.RichModel;
+import org.sosy_lab.cpachecker.core.interfaces.Property;
 import org.sosy_lab.cpachecker.util.AbstractStates;
 import org.sosy_lab.cpachecker.util.CFAUtils;
 import org.sosy_lab.cpachecker.util.SourceLocationMapper;
@@ -110,6 +112,7 @@ import com.google.common.base.Predicates;
 import com.google.common.base.Strings;
 import com.google.common.base.Supplier;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
 import com.google.common.collect.Iterables;
@@ -377,7 +380,7 @@ public class ARGPathExporter {
   private class WitnessWriter implements EdgeAppender {
 
     private final Multimap<String, NodeFlag> nodeFlags = TreeMultimap.create();
-    private final Multimap<String, String> violatedProperties = TreeMultimap.create();
+    private final Multimap<String, Property> violatedProperties = HashMultimap.create();
     private final Map<DelayedAssignmentsKey, CFAEdgeWithAssumptions> delayedAssignments = Maps.newHashMap();
 
     private final Multimap<String, Edge> leavingEdges = TreeMultimap.create();
@@ -1000,8 +1003,8 @@ public class ARGPathExporter {
       for (NodeFlag f : nodeFlags.get(pEntryStateNodeId)) {
         pDoc.addDataElementChild(result, f.key, "true");
       }
-      for (String violation : violatedProperties.get(pEntryStateNodeId)) {
-        pDoc.addDataElementChild(result, KeyDef.VIOLATEDPROPERTY, violation);
+      for (Property violation : violatedProperties.get(pEntryStateNodeId)) {
+        pDoc.addDataElementChild(result, KeyDef.VIOLATEDPROPERTY, violation.toString());
       }
       pDoc.appendToAppendable(result);
     }
@@ -1013,15 +1016,17 @@ public class ARGPathExporter {
       return Collections.emptySet();
     }
 
-    private Collection<String> extractViolatedProperties(ARGState pState) {
+    private Collection<Property> extractViolatedProperties(ARGState pState) {
+      ArrayList<Property> result = Lists.newArrayList();
       if (pState.isTarget()) {
-        String violatedPropertyDescription = pState.getViolatedPropertyDescription();
-        int pos = violatedPropertyDescription.indexOf(':');
-        if (pos >= 0) {
-          return Collections.singleton(violatedPropertyDescription.substring(0, pos));
-        }
+        result.addAll(pState.getViolatedProperties());
+        // TODO!!! What is the purpose of the following code???
+        //int pos = violatedPropertyDescription.indexOf(':');
+        //if (pos >= 0) {
+        // return Collections.singleton(violatedPropertyDescription.substring(0, pos));
+        //}
       }
-      return Collections.emptySet();
+      return result;
     }
   }
 

src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonGraphmlParser.java
@@ -641,20 +641,30 @@ public class AutomatonGraphmlParser {
     public String getViolatedPropertyDescription(AutomatonExpressionArguments pArgs) {
       String own = getFollowState().isTarget() ? super.getViolatedPropertyDescription(pArgs) : null;
       List<String> violatedPropertyDescriptions = new ArrayList<>();
+
       if (!Strings.isNullOrEmpty(own)) {
         violatedPropertyDescriptions.add(own);
       }
+
       for (AutomatonState other : FluentIterable.from(pArgs.getAbstractStates()).filter(AutomatonState.class)) {
         if (other != pArgs.getState() && other.getInternalState().isTarget()) {
-          Optional<String> violatedPropertyDescription = other.getOptionalViolatedPropertyDescription();
-          if (violatedPropertyDescription.isPresent() && !violatedPropertyDescription.get().isEmpty()) {
-            violatedPropertyDescriptions.add(violatedPropertyDescription.get());
+          String violatedPropDesc = "";
+
+          Optional<AutomatonSafetyProperty> violatedProperty = other.getOptionalViolatedPropertyDescription();
+          if (violatedProperty.isPresent()) {
+            violatedPropDesc = violatedProperty.get().toString();
+          }
+
+          if (!violatedPropDesc.isEmpty()) {
+            violatedPropertyDescriptions.add(violatedPropDesc);
           }
         }
       }
+
       if (violatedPropertyDescriptions.isEmpty() && own == null) {
         return null;
       }
+
       return Joiner.on(',').join(violatedPropertyDescriptions);
     }
 

commit 01463a2b1360338501f2f497bddeddfff7d377c8

Fixed a bug introduced in r18480 which broke the propagation of the violated-property description.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@20776 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonTransferRelation.java
@@ -27,6 +27,7 @@ import static com.google.common.base.Predicates.instanceOf;
 import static com.google.common.collect.FluentIterable.from;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -184,7 +185,9 @@ class AutomatonTransferRelation extends SingleEdgeTransferRelation {
 
           } else {
             // matching transitions, but unfulfilled assertions: goto error state
-            AutomatonSafetyProperty prop = new AutomatonSafetyProperty(state.getOwningAutomaton(), t);
+            final String desc = Strings.nullToEmpty(t.getViolatedPropertyDescription(exprArgs));
+            AutomatonSafetyProperty prop =
+                new AutomatonSafetyProperty(state.getOwningAutomaton(), t, desc);
 
             AutomatonState errorState = AutomatonState.automatonStateFactory(
                 Collections.<String, AutomatonVariable>emptyMap(), AutomatonInternalState.ERROR, cpa, 0, 0, prop);
@@ -219,7 +222,7 @@ class AutomatonTransferRelation extends SingleEdgeTransferRelation {
 
         AutomatonSafetyProperty violatedProperty = null;
         if (t.getFollowState().isTarget()) {
-          final String desc = t.getViolatedPropertyDescription(exprArgs);
+          final String desc = Strings.nullToEmpty(t.getViolatedPropertyDescription(exprArgs));
           violatedProperty = new AutomatonSafetyProperty(state.getOwningAutomaton(), t, desc);
         }
 