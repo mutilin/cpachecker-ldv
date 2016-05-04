mutilin/cpachecker-ldv

commit a5df3f18625ebdd86ef189493329a03011531fc0

Write JSON strings directly into the report.
See #244

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@21039 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cfa/export/DOTBuilder2.java
@@ -23,6 +23,7 @@
  */
 package org.sosy_lab.cpachecker.cfa.export;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static org.sosy_lab.cpachecker.util.CFAUtils.successorsOf;
 
@@ -74,22 +75,43 @@ public final class DOTBuilder2 {
   public final static Path F_CALL_EDGES = Paths.get("fcalledges.json");
   public final static Path CFA_INFO = Paths.get("cfainfo.json");
 
-  private DOTBuilder2() { /* utility class */ }
+  private final CFA cfa;
+  private final CFAJSONBuilder jsoner;
+  private final DOTViewBuilder dotter;
+
+  public DOTBuilder2(CFA pCfa) {
+    cfa = checkNotNull(pCfa);
+    jsoner = new CFAJSONBuilder();
+    dotter = new DOTViewBuilder(cfa);
+    CFAVisitor vis = new NodeCollectingCFAVisitor(new CompositeCFAVisitor(jsoner, dotter));
+    for (FunctionEntryNode entryNode : cfa.getAllFunctionHeads()) {
+      CFATraversal.dfs().ignoreFunctionCalls().traverse(entryNode, vis);
+    }
+  }
 
   /**
    * output the CFA as DOT and JSON files
    */
-  public static void writeReport(CFA cfa, Path outdir) throws IOException {
-    CFAJSONBuilder jsoner = new CFAJSONBuilder();
-    DOTViewBuilder dotter = new DOTViewBuilder(cfa);
-    CFAVisitor vis = new NodeCollectingCFAVisitor(new CompositeCFAVisitor(jsoner, dotter));
+  public void writeReport(Path outdir) throws IOException {
     for (FunctionEntryNode entryNode : cfa.getAllFunctionHeads()) {
-      CFATraversal.dfs().ignoreFunctionCalls().traverse(entryNode, vis);
       dotter.writeFunctionFile(entryNode.getFunctionName(), outdir);
     }
     dotter.writeGlobalFiles(outdir);
     JSON.writeJSONString(jsoner.getJSON(), outdir.resolve(CFA_INFO));
   }
+
+  public void writeCfaInfo(Writer out) throws IOException {
+    JSON.writeJSONString(jsoner.getJSON(), out);
+  }
+
+  public void writeFunctionCallEdges(Writer out) throws IOException {
+    JSON.writeJSONString(dotter.virtFuncCallEdges, out);
+  }
+
+  public  void writeCombinedNodes(Writer out) throws IOException {
+    JSON.writeJSONString(dotter.node2combo, out);
+  }
+
 
   private static String getEdgeText(CFAEdge edge) {
     //the first call to replaceAll replaces \" with \ " to prevent a bug in dotty.

src/org/sosy_lab/cpachecker/cfa/CFACreator.java
@@ -970,7 +970,7 @@ v.addInitializer(initializer);
     if (exportCfaPerFunction && exportCfaFile != null) {
       try {
         Path outdir = exportCfaFile.getParent();
-        DOTBuilder2.writeReport(cfa, outdir);
+        new DOTBuilder2(cfa).writeReport(outdir);
       } catch (IOException e) {
         logger.logUserException(Level.WARNING, e,
           "Could not write CFA to dot and json file");

src/org/sosy_lab/cpachecker/core/counterexample/GenerateReportWithoutGraphs.java
@@ -50,6 +50,7 @@ public class GenerateReportWithoutGraphs {
   private final Configuration config;
   private final LogManager logger;
   private final CFA cfa;
+  private final DOTBuilder2 dotBuilder;
 
   @Option(
     secure = true,
@@ -68,9 +69,6 @@ public class GenerateReportWithoutGraphs {
 
   @Nullable private final Path outputPath;
   @Nullable private final Path reportDir;
-  @Nullable private final Path combinedNodesPath;
-  @Nullable private final Path cfaInfoPath;
-  @Nullable private final Path fCallEdgesPath;
 
   private final List<Path> errorPathFiles = new ArrayList<>();
   private final List<String> sourceFiles = new ArrayList<>();
@@ -80,6 +78,7 @@ public class GenerateReportWithoutGraphs {
     config = checkNotNull(pConfig);
     logger = checkNotNull(pLogger);
     cfa = checkNotNull(pCfa);
+    dotBuilder = new DOTBuilder2(pCfa);
 
     config.inject(this);
     if (statisticsFile != null) {
@@ -91,19 +90,12 @@ public class GenerateReportWithoutGraphs {
     }
 
     if (outputPath != null) {
-      combinedNodesPath = outputPath.resolve(DOTBuilder2.COMBINED_NODES);
-      fCallEdgesPath = outputPath.resolve(DOTBuilder2.F_CALL_EDGES);
-      cfaInfoPath = outputPath.resolve(DOTBuilder2.CFA_INFO);
-
       reportDir = outputPath.resolve(REPORT);
 
       sourceFiles.addAll(COMMA_SPLITTER.splitToList(programs));
       errorPathFiles.addAll(getErrorPathFiles());
 
     } else {
-      combinedNodesPath = null;
-      fCallEdgesPath = null;
-      cfaInfoPath = null;
       reportDir = null;
     }
   }
@@ -310,66 +302,22 @@ public class GenerateReportWithoutGraphs {
     }
   }
 
-  private void insertFCallEdges(BufferedWriter bufferedWriter) {
-    if (fCallEdgesPath != null && fCallEdgesPath.exists()) {
-      try (BufferedReader bufferedReader =
-          new BufferedReader(
-              new InputStreamReader(
-                  new FileInputStream(fCallEdgesPath.toFile()), Charset.defaultCharset()))) {
-        String line;
-        bufferedWriter.write("var fCallEdges = ");
-        while (null != (line = bufferedReader.readLine())) {
-          bufferedWriter.write(line);
-        }
-        bufferedWriter.write(";\n");
-      } catch (IOException e) {
-        logger.logUserException(
-            WARNING, e, "Could not create report: Insertion of function call edges failed.");
-      }
-    }
+  private void insertFCallEdges(BufferedWriter report) throws IOException {
+    report.write("var fCallEdges = ");
+    dotBuilder.writeFunctionCallEdges(report);
+    report.write(";\n");
   }
 
-  private void insertCombinedNodesData(Writer report) {
-
-    if (combinedNodesPath != null && combinedNodesPath.exists()) {
-      try (BufferedReader combinedNodes =
-          new BufferedReader(
-              new InputStreamReader(
-                  new FileInputStream(combinedNodesPath.toFile()), Charset.defaultCharset()))) {
-
-        String line;
-        report.write("var combinedNodes = ");
-        while (null != (line = combinedNodes.readLine())) {
-          report.write(line);
-        }
-        report.write(";\n");
-
-      } catch (IOException e) {
-        logger.logUserException(
-            WARNING, e, "Could not create report: Insertion of combindes nodes failed.");
-      }
-    }
+  private void insertCombinedNodesData(Writer report) throws IOException {
+    report.write("var combinedNodes = ");
+    dotBuilder.writeCombinedNodes(report);
+    report.write(";\n");
   }
 
-  private void insertCfaInfoData(Writer report) {
-    if (cfaInfoPath != null && cfaInfoPath.exists()) {
-      try (BufferedReader bufferedReader =
-          new BufferedReader(
-              new InputStreamReader(
-                  new FileInputStream(cfaInfoPath.toFile()), Charset.defaultCharset()))) {
-
-        String line;
-        report.write("var cfaInfo = ");
-        while (null != (line = bufferedReader.readLine())) {
-          report.write(line);
-        }
-        report.write(";\n");
-
-      } catch (IOException e) {
-        logger.logUserException(
-            WARNING, e, "Could not create report: Insertion of CFA info failed.");
-      }
-    }
+  private void insertCfaInfoData(Writer report) throws IOException {
+    report.write("var cfaInfo = ");
+    dotBuilder.writeCfaInfo(report);
+    report.write(";\n");
   }
 
   private void insertErrorPathData(Path errorPatData, BufferedWriter bufferedWriter) {

commit 63e5b2274fdec490594e2a9c1ee1cd109b884b60

Fix bug introduced in r21039: The CFAs of all functions were exported into one file and the other files contained empty graphs.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@21056 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cfa/export/DOTBuilder2.java
@@ -29,8 +29,11 @@ import static org.sosy_lab.cpachecker.util.CFAUtils.successorsOf;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
+import com.google.common.collect.MultimapBuilder.ListMultimapBuilder;
+import com.google.common.collect.MultimapBuilder.SetMultimapBuilder;
+import com.google.common.collect.SetMultimap;
 
 import org.sosy_lab.common.JSON;
 import org.sosy_lab.common.io.Path;
@@ -53,7 +56,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Generates one DOT file per function for the report.
@@ -134,9 +136,12 @@ public final class DOTBuilder2 {
     private int virtFuncCallNodeIdCounter = 100000;
 
     // local state per function
-    private final Set<CFANode> nodes = Sets.newLinkedHashSet();
-    private final List<CFAEdge> edges = Lists.newArrayList();
-    private final List<List<CFAEdge>> comboedges = Lists.newArrayList();
+    private final SetMultimap<String, CFANode> nodes =
+        SetMultimapBuilder.hashKeys().linkedHashSetValues().build();
+    private final ListMultimap<String, CFAEdge> edges =
+        ListMultimapBuilder.hashKeys().arrayListValues().build();
+    private final ListMultimap<String, List<CFAEdge>> comboedges =
+        ListMultimapBuilder.hashKeys().arrayListValues().build();
 
     private List<CFAEdge> currentComboEdge = null;
 
@@ -149,6 +154,7 @@ public final class DOTBuilder2 {
     @Override
     public TraversalProcess visitEdge(CFAEdge edge) {
       CFANode predecessor = edge.getPredecessor();
+      String funcname = predecessor.getFunctionName();
 
       // check if it qualifies for a comboEdge
       if (    predecessor.isLoopStart()
@@ -159,18 +165,18 @@ public final class DOTBuilder2 {
           || (edge.getEdgeType() == CFAEdgeType.AssumeEdge)) {
         // no, it does not
 
-        edges.add(edge);
+        edges.put(funcname, edge);
         currentComboEdge = null;
 
         // nodes are only added if they are not hidden by a comboedge
-        nodes.add(predecessor);
-        nodes.add(edge.getSuccessor());
+        nodes.put(funcname, predecessor);
+        nodes.put(funcname, edge.getSuccessor());
 
       } else {
         // add combo edge
         if (currentComboEdge == null) {
           currentComboEdge = Lists.newArrayList();
-          comboedges.add(currentComboEdge);
+          comboedges.put(funcname, currentComboEdge);
         }
         currentComboEdge.add(edge);
       }
@@ -180,47 +186,48 @@ public final class DOTBuilder2 {
 
     void writeFunctionFile(String funcname, Path outdir) throws IOException {
 
-        try (Writer out = outdir.resolve("cfa__" + funcname + ".dot").asCharSink(StandardCharsets.UTF_8).openBufferedStream()) {
-          out.write("digraph " + funcname + " {\n");
-          StringBuilder outb = new StringBuilder();
-          //write comboedges
-          for (List<CFAEdge> combo: comboedges) {
-            if (combo.size() == 1) {
-              edges.add(combo.get(0));
-              nodes.add(combo.get(0).getPredecessor());
-              nodes.add(combo.get(0).getSuccessor());
+      try (Writer out =
+          outdir
+              .resolve("cfa__" + funcname + ".dot")
+              .asCharSink(StandardCharsets.UTF_8)
+              .openBufferedStream()) {
+        out.write("digraph " + funcname + " {\n");
+        StringBuilder outb = new StringBuilder();
 
-            } else {
-              outb.append(comboToDot(combo));
+        //write comboedges
+        for (List<CFAEdge> combo : comboedges.get(funcname)) {
+          if (combo.size() == 1) {
+            edges.put(funcname, combo.get(0));
+            nodes.put(funcname, combo.get(0).getPredecessor());
+            nodes.put(funcname, combo.get(0).getSuccessor());
 
-              CFAEdge first = combo.get(0);
-              CFAEdge last = combo.get(combo.size() - 1);
+          } else {
+            outb.append(comboToDot(combo));
 
-              outb.append(first.getPredecessor().getNodeNumber());
-              outb.append(" -> ");
-              outb.append(last.getSuccessor().getNodeNumber());
-              outb.append("[label=\"\"]\n");
-            }
+            CFAEdge first = combo.get(0);
+            CFAEdge last = combo.get(combo.size() - 1);
+
+            outb.append(first.getPredecessor().getNodeNumber());
+            outb.append(" -> ");
+            outb.append(last.getSuccessor().getNodeNumber());
+            outb.append("[label=\"\"]\n");
           }
-
-          //write nodes
-          for (CFANode node: nodes) {
-            out.write(DOTBuilder.formatNode(node, loopHeads));
-            out.write('\n');
-          }
-
-          out.write(outb.toString());
-
-          //write edges
-          for (CFAEdge edge: edges) {
-            out.write(edgeToDot(edge));
-          }
-          out.write("}");
-
-          nodes.clear();
-          edges.clear();
-          comboedges.clear();
         }
+
+        //write nodes
+        for (CFANode node : nodes.get(funcname)) {
+          out.write(DOTBuilder.formatNode(node, loopHeads));
+          out.write('\n');
+        }
+
+        out.write(outb.toString());
+
+        //write edges
+        for (CFAEdge edge : edges.get(funcname)) {
+          out.write(edgeToDot(edge));
+        }
+        out.write("}");
+      }
     }
 
     void writeGlobalFiles(Path outdir) throws IOException {