mutilin/cpachecker-ldv

commit 889c83d8cc4537a67e46ec9f20bd797bf2117961

Fixed a bug in the statistics collection of BMCAlgorithm, where exporting a correctness witness using the invariant generator failed with an IllegalStateException when the algorithm terminated before the invariant generator was started.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22763 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/invariants/AbstractInvariantGenerator.java
@@ -27,11 +27,18 @@ import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.reachedset.AggregatedReachedSets;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class AbstractInvariantGenerator implements InvariantGenerator {
 
+  private AtomicBoolean started = new AtomicBoolean();
+
   @Override
-  public abstract void start(CFANode pInitialLocation);
+  public final void start(CFANode pInitialLocation) {
+    started.set(true);
+  }
+
+  protected abstract void startImpl(CFANode pInitialLocation);
 
   @Override
   public abstract void cancel();
@@ -43,5 +50,10 @@ public abstract class AbstractInvariantGenerator implements InvariantGenerator {
 
   @Override
   public abstract boolean isProgramSafe();
+
+  @Override
+  public boolean isStarted() {
+    return started.get();
+  }
 
 }

src/org/sosy_lab/cpachecker/core/algorithm/bmc/AbstractBMCAlgorithm.java
@@ -230,7 +230,7 @@ abstract class AbstractBMCAlgorithm implements StatisticsProvider {
           new AbstractInvariantGenerator() {
 
             @Override
-            public void start(CFANode pInitialLocation) {
+            protected void startImpl(CFANode pInitialLocation) {
               // do nothing
             }
 

src/org/sosy_lab/cpachecker/core/algorithm/bmc/BMCAlgorithm.java
@@ -332,19 +332,24 @@ public class BMCAlgorithm extends AbstractBMCAlgorithm implements Algorithm {
             ARGState rootState =
                 AbstractStates.extractStateByType(pReached.getFirstState(), ARGState.class);
             if (rootState != null && invariantsExport != null) {
-              ExpressionTreeSupplier tmp;
-              try {
-                if (invariantGenerator instanceof KInductionInvariantGenerator) {
-                  tmp =
-                      ((KInductionInvariantGenerator) invariantGenerator)
-                          .getExpressionTreeSupplier();
-                } else {
-                  tmp = new ExpressionTreeInvariantSupplier(invariantGenerator.get(), cfa);
+              ExpressionTreeSupplier tmpExpressionTreeSupplier =
+                  ExpressionTreeSupplier.TrivialInvariantSupplier.INSTANCE;
+              if (invariantGenerator.isStarted()) {
+                try {
+                  if (invariantGenerator instanceof KInductionInvariantGenerator) {
+                    tmpExpressionTreeSupplier =
+                        ((KInductionInvariantGenerator) invariantGenerator)
+                            .getExpressionTreeSupplier();
+                  } else {
+                    tmpExpressionTreeSupplier =
+                        new ExpressionTreeInvariantSupplier(invariantGenerator.get(), cfa);
+                  }
+                } catch (CPAException | InterruptedException e1) {
+                  tmpExpressionTreeSupplier =
+                      ExpressionTreeSupplier.TrivialInvariantSupplier.INSTANCE;
                 }
-              } catch (CPAException | InterruptedException e1) {
-                tmp = ExpressionTreeSupplier.TrivialInvariantSupplier.INSTANCE;
               }
-              final ExpressionTreeSupplier expSup = tmp;
+              final ExpressionTreeSupplier expSup = tmpExpressionTreeSupplier;
 
               try (Writer w = MoreFiles.openOutputFile(invariantsExport, StandardCharsets.UTF_8)) {
                 argPathExporter.writeProofWitness(

src/org/sosy_lab/cpachecker/core/algorithm/invariants/CPAInvariantGenerator.java
@@ -238,7 +238,7 @@ public class CPAInvariantGenerator extends AbstractInvariantGenerator implements
   }
 
   @Override
-  public void start(final CFANode initialLocation) {
+  protected void startImpl(final CFANode initialLocation) {
     checkState(invariantGenerationFuture == null);
 
     Callable<AggregatedReachedSets> task = new InvariantGenerationTask(initialLocation);

src/org/sosy_lab/cpachecker/core/algorithm/invariants/DoNothingInvariantGenerator.java
@@ -32,7 +32,7 @@ import org.sosy_lab.cpachecker.cfa.model.CFANode;
 public class DoNothingInvariantGenerator extends AbstractInvariantGenerator {
 
   @Override
-  public void start(CFANode pInitialLocation) { }
+  protected void startImpl(CFANode pInitialLocation) {}
 
   @Override
   public void cancel() { }

src/org/sosy_lab/cpachecker/core/algorithm/invariants/InvariantGenerator.java
@@ -47,6 +47,14 @@ import org.sosy_lab.cpachecker.util.predicates.invariants.FormulaInvariantsSuppl
 public interface InvariantGenerator {
 
   /**
+   * Checks if the invariant generator has already been started.
+   *
+   * @return {@code true} if the invariant generator has already been started,
+   * {@code false} otherwise.
+   */
+  boolean isStarted();
+
+  /**
    * Prepare invariant generation, and optionally start the algorithm.
    * May be called only once.
    */

src/org/sosy_lab/cpachecker/core/algorithm/invariants/KInductionInvariantGenerator.java
@@ -364,7 +364,7 @@ public class KInductionInvariantGenerator extends AbstractInvariantGenerator imp
   }
 
   @Override
-  public void start(final CFANode initialLocation) {
+  protected void startImpl(final CFANode initialLocation) {
     checkState(invariantGenerationFuture == null);
 
     Callable<Pair<InvariantSupplier, ExpressionTreeSupplier>> task =

commit 276df018093e2e78c41693277eb1bb357b49da1b

Fix a bug introduced in r22763 where invariant generators were never really started.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22774 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/invariants/AbstractInvariantGenerator.java
@@ -35,6 +35,7 @@ public abstract class AbstractInvariantGenerator implements InvariantGenerator {
 
   @Override
   public final void start(CFANode pInitialLocation) {
+    startImpl(pInitialLocation);
     started.set(true);
   }
 